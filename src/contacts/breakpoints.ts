1222891a8465b073818b8beda46e4346c057a728
Emilian Timofei <emilianmail96@gmail.com>
2025-07-05 15:03:47 +0200

    da capo

--- src/contacts/breakpoints.ts
+++
@@ -1,8 +0,0 @@
-export const BREAKPOINTS = {
-  MOBILE: 480,
-  TABLET: 768,
-  DESKTOP: 1024,
-  XL_DESKTOP: 1280,
-} as const;
-
-export type Breakpoint = keyof typeof BREAKPOINTS;
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./assets/styles/main.scss"; // Crea questo file
-
-createApp(App).mount("#app");
